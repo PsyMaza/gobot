4460f1780017d261fb905c0b34040b78b1f31180
Евгений Иванов <psymaza@gmail.com>
2022-04-12 23:26:48 +0300

    Create bot

--- cmd/gobot/main.go
+++ cmd/gobot/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
 
+const token = "GOBOT_TOKEN"
+
+func main() {
+	godotenv.Load()
+	key := os.Getenv(token)
+	bot, err := tgbotapi.NewBotAPI(key)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil {
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
 }
